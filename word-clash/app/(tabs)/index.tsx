2d18c2fd1e346e6d37a061582d5973a77c3a9704
Andrei Ionescu <129840688+ionescuaandrei@users.noreply.github.com>
2025-04-05 16:06:33 +0300

    +

--- word-clash/app/(tabs)/index.tsx
+++ word-clash/app/(tabs)/index.tsx
@@ -1,12 +1,46 @@
-import { View, Text } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { COLORS } from '@/constants/theme'
 
-const battle = () => {
+const index = () => {
+
+  const [firstMatch, setFirstMatch] = React.useState(false)
+
   return (
-    <View>
-      <Text>battle</Text>
+    <View style={styles.container}>
+      <View>
+        {
+          firstMatch ? (
+            <Text>First match</Text>
+          ) : (
+            <Text>Not first match</Text>
+          )
+        }
+        <Text style={styles.findMatch}></Text>
+        <Text>Play a 1v1 match</Text>
+        <TouchableOpacity>
+          <Text>Find match</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
-export default battle
+export default index
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: COLORS.background,
+      // color: COLORS.white, // color prop is not valid on View
+    },
+    findMatch: {
+      fontSize: 30,
+      fontWeight: "bold",
+      color: COLORS.white,
+      marginBottom: 10,
+    },
+})
